e5ab51bc140317141fbd6b6e926751af86ceb9ed
yosef plishtiiv <yosef014@gmail.com>
2022-03-21 16:02:48 +0200

    hedaer

--- src/components/app-header.cmp.vue
+++ src/components/app-header.cmp.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="nav-container" ref="nav">
-    <div class="logo">fiiver.</div>
-    <div class="nav">
-      <ul>
-        <li>Fiverr Business</li>
-        <li>Explore</li>
-        <li>English</li>
-        <li>ILS</li>
-        <li>Become a Seller</li>
-        <li>Sign in</li>
-        <li class="join">Join</li>
-      </ul>
-      <!-- <button @click="log">xx</button> -->
-      {{ navContainer }}
+  <div class="header-container-fluid fluid-el">
+    <div class="nav-container" ref="nav">
+      <div class="logo">fiiver.</div>
+      <div class="nav">
+        <ul>
+          <li>Fiverr Business</li>
+          <li>Explore</li>
+          <li>English</li>
+          <li>ILS</li>
+          <li>Become a Seller</li>
+          <li>Sign in</li>
+          <li class="join">Join</li>
+        </ul>
+        <!-- <button @click="log">xx</button> -->
+      </div>
     </div>
   </div>
 </template>
@@ -23,22 +24,25 @@ export default {
     log() {
       // console.log(this.$refs.nav);
       console.log("hii");
-      this.navContainer = "nav-container-open-one";
     },
   },
 
   mounted() {
     const elNav = document.querySelector(".nav-container");
+    const elNavFluid = document.querySelector(".fluid-el");
     window.addEventListener("scroll", function () {
       if (window.scrollY > 200) {
         console.log("200");
-        this.navContainer = "nav-container-open-one";
 
         // console.log(elNav);
       } else if (window.scrollY > 50) {
         elNav.classList.remove("nav-container");
         elNav.classList.add("nav-container-open-one");
+        elNavFluid.classList.add("header-container-fluid-close");
+        elNavFluid.classList.remove("header-container-fluid");
       } else if (window.scrollY > 0) {
+        elNavFluid.classList.remove("header-container-fluid-close");
+        elNavFluid.classList.add("header-container-fluid");
         elNav.classList.remove("nav-container-open-one");
         elNav.classList.add("nav-container");
       }
