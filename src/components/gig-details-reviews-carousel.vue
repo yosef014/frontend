c9adfaca43fd840dc8bdf9c3734840965d77161e
sowhat1234 <emiliu1234e@gmail.com>
2022-03-24 17:14:20 +0200

    push first push

---
+++ src/components/gig-details-reviews-carousel.vue
@@ -0,0 +1,79 @@
+<template>
+  <section class="gig-reviews-carousel-section">
+    <div class="gig-reviews-carousel">
+      <header class="gig-reviews-carousel-header">
+        <h2 class="gig-section-title">What people loved about this seller</h2>
+        <button class="gig-see-all-reviews-btn">
+          See all reviews //href to reviews
+        </button>
+      </header>
+    </div>
+    <Carousel :settings="settings" :breakpoints="breakpoints">
+      <Slide v-for="gigReview in gig.reviewers" :key="gigReview">
+        <div class="gig-carousel__item">
+          <div class="gig-review-user-profile-img">
+            <img src="" alt="" />
+          </div>
+          <div class="gig-carousel-details">
+            <div class="gig-review-header">
+              <h6 class="gig-review-name">{{ gigReview.name }}</h6>
+              <div class="gig-review-country-flag">
+                <img :src="gigReview.flag" alt="" />
+                <div class="gig-review-country-name">
+                  {{ gigReview.country }}
+                </div>
+              </div>
+            </div>
+            <p class="gig-review-description">
+              {{ gigReview.review }}
+            </p>
+          </div>
+        </div>
+      </Slide>
+
+      <template #addons>
+        <Navigation />
+      </template>
+    </Carousel>
+  </section>
+</template>
+
+<script>
+  import { Carousel, Navigation, Slide, Pagination } from "vue3-carousel";
+  import "vue3-carousel/dist/carousel.css";
+  export default {
+    name: "ExamplePagination",
+    props: {
+      gig: Object,
+    },
+    components: {
+      Pagination,
+      Carousel,
+      Slide,
+      Navigation,
+    },
+    data() {
+      return {
+        // carousel settings
+        settings: {
+          itemsToShow: 1,
+          snapAlign: "center",
+        },
+        // breakpoints are mobile first
+        // any settings not specified will fallback to the carousel settings
+        breakpoints: {
+          // 700px and up
+          700: {
+            itemsToShow: 3.5,
+            snapAlign: "center",
+          },
+          // 1024 and up
+          1024: {
+            itemsToShow: 1,
+            snapAlign: "center",
+          },
+        },
+      };
+    },
+  };
+</script>
--- src/views/gig-details.vue
+++ src/views/gig-details.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="gig-page">
+  <div v-if="gig" class="gig-page">
     <section class="gig-sidebar">
       <div class="gig-purchase-tab">
-        <form class="gig-content">
+        <form class="gig-purchase-content">
           <h3>
             <b class="gig-title">Silver</b>
+            <span class="gig-price">{{ gig.price }}</span>
           </h3>
           <p>3 Unique Concepts + Color Palette + Source files + JPG, PNG</p>
           <span class="gig-delivery-icon"> </span>
@@ -21,7 +22,9 @@
           </ul>
           <footer class="gig-footer">
             <button class="gig-purchase-btn">
-              Continue <span>(enter price here)</span>
+              <rotuer-link to="/">
+                Continue <span>({{ gig.price }})</span>
+              </rotuer-link>
             </button>
           </footer>
         </form>
@@ -31,7 +34,6 @@
         <button class="gig-contact-seller-btn">Contact Seller</button>
       </div>
     </section>
-    <login></login>
     <div class="gig-main">
       <div class="gig-overview">
         <nav class="gig-categories-breadcrumbs">
@@ -50,7 +52,7 @@
           </ul>
         </nav>
         <h1 class="gig-text-display">
-          I will design modern logo with brand identity in 24 hrs
+          {{ gig.title }}
         </h1>
         <div class="gig-seller-overview">
           <div class="gig-seller-pic">
@@ -63,6 +65,7 @@
             />
           </div>
           <div class="gig-seller-details">
+            <div class="gig-seller-name">{{ gig.fullname }}</div>
             <span class="gig-seller-level"
               ><div class="gig-seller-level-div">Level 2 Seller</div></span
             >
@@ -82,106 +85,35 @@
         </div>
       </div>
       <section class="gig-gallery-component">
-        <Carousel :settings="settingsGallerySlideshow">
-          <Slide v-for="img in gigCarouselImgs" :key="img">
-            <div class="gig-details-carousel">
-              <img :src="img.img" alt="" />
-            </div>
-          </Slide>
-
-          <template #addons>
-            <Navigation />
-          </template>
-        </Carousel>
+        <gig-details-gallery-carousel :gig="gig" />
       </section>
-      <div class="gig-reviews-snippet-carousel">
-        <header>
-          <h2 class="gig-section-title">What people loved about this seller</h2>
-          <button class="gig-see-all-reviews-btn">
-            See all reviews //href to reviews
-          </button>
-        </header>
-        <span class="gig-reviews-carousel-wrapper">
-          <Carousel :settings="settingsReviewSlideshow">
-            <Slide v-for="review in gigReviews" :key="review">
-              <div class="gig-details-carousel">
-                <p>{{ review.name }}</p>
-                {{ review.review }}
-              </div>
-            </Slide>
+      <section class="gig-reviews-carousel-component">
+        <gig-details-reviews-carousel :gig="gig" />
+      </section>
 
-            <template #addons>
-              <Navigation />
-            </template>
-          </Carousel>
-        </span>
-      </div>
       <div class="gig-description">
         <header><h2 class="gig-section-title">About This Gig</h2></header>
-        <div class="gig-description-content"></div>
+        <div class="gig-description-content">
+          {{ gig.description }}
+        </div>
         <!-- <button class="collapse-button">+ See More</button> -->
       </div>
     </div>
   </div>
-  <pre>
-TEST TEST TEST TESTTEST TEST TEST TESTTEST TEST TEST TESTTEST TEST TEST TEST </pre
-  >
 </template>
 
 <script>
   import { gigService } from "../services/gig-service";
-  import { Carousel, Navigation, Slide } from "vue3-carousel";
-  import login from "../components/login.vue";
+  import gigDetailsGalleryCarousel from "../components/gig-details-gallery-carousel.vue";
+  import gigDetailsReviewsCarousel from "../components/gig-details-reviews-carousel.vue";
   export default {
     components: {
-      Carousel,
-      Slide,
-      Navigation,
-      login,
+      gigDetailsGalleryCarousel,
+      gigDetailsReviewsCarousel,
     },
     data() {
       return {
-        settingsGallerySlideshow: {
-          itemsToShow: 1,
-          transition: 600,
-          snapAlign: "center",
-          wrapAround: true,
-        },
-        settingsReviewSlideshow: {
-          itemsToShow: 1,
-          transition: 600,
-          snapAlign: "center",
-          wrapAround: true,
-        },
         gig: null,
-        gigCarouselImgs: [
-          {
-            img: "https://fiverr-res.cloudinary.com/t_gig_cards_web,q_auto,f_auto/gigs/231682055/original/77cc585046a1ceb81a809218fef35ee8252bbb3b.jpg",
-          },
-          {
-            img: "https://fiverr-res.cloudinary.com/t_gig_cards_web,q_auto,f_auto/gigs/231682055/original/77cc585046a1ceb81a809218fef35ee8252bbb3b.jpg",
-          },
-          {
-            img: "https://fiverr-res.cloudinary.com/t_gig_cards_web,q_auto,f_auto/gigs/231682055/original/77cc585046a1ceb81a809218fef35ee8252bbb3b.jpg",
-          },
-        ],
-        gigReviews: [
-          {
-            review:
-              "Use your revisions and communication, and you will have something that works for you! I recommend modernmarvel for the price they ask! I did not know what to expect from my first buy on FIverr. The previews where what I was going for, so I thought why not give it a try. The initial delivery had two good concepts and three concepts I did not like. The two good concepts however, where not really what I wanted though. I submitted a revision proposal and hoped for the best. This is where this seller shines! From the initial designs, he worked quickly with every suggestion I made for revisions and was good in communication. I slowly saw my project evolving to something I love. Recommended!",
-            name: "bartstrijbos",
-          },
-          {
-            review:
-              "Use your revisions and communication, and you will have something that works for you! I recommend modernmarvel for the price they ask! I did not know what to expect from my first buy on FIverr. The previews where what I was going for, so I thought why not give it a try. The initial delivery had two good concepts and three concepts I did not like. The two good concepts however, where not really what I wanted though. I submitted a revision proposal and hoped for the best. This is where this seller shines! From the initial designs, he worked quickly with every suggestion I made for revisions and was good in communication. I slowly saw my project evolving to something I love. Recommended!",
-            name: "bartstrijbos",
-          },
-          {
-            review:
-              "Use your revisions and communication, and you will have something that works for you! I recommend modernmarvel for the price they ask! I did not know what to expect from my first buy on FIverr. The previews where what I was going for, so I thought why not give it a try. The initial delivery had two good concepts and three concepts I did not like. The two good concepts however, where not really what I wanted though. I submitted a revision proposal and hoped for the best. This is where this seller shines! From the initial designs, he worked quickly with every suggestion I made for revisions and was good in communication. I slowly saw my project evolving to something I love. Recommended!",
-            name: "bartstrijbos",
-          },
-        ],
       };
     },
     async created() {
