f3fa85ac00a604b6c3fd28b51bf0d8a8668700ac
BG-Guy <53979740+BG-Guy@users.noreply.github.com>
2022-03-26 14:53:00 +0300

    finished purchase summary styling

--- src/views/gig-details.vue
+++ src/views/gig-details.vue
@@ -1,4 +1,11 @@
 <template>
+  <nav :style="setSticky" class="gig-details-navbar-container">
+    <div class="gig-details-navbar max-width-container">
+      <li :class="{'gig-nav-link-active' : link.active === true}" v-for="(link,idx) in gigDetailsNavLinks" :key="link.name">
+        <a @click="scrollTo(link.section, idx)">{{ link.name }}</a>
+      </li>
+    </div>
+  </nav>
   <div class="page-content-container">
     <div v-if="gig" class="gig-page">
       <section class="gig-sidebar">
@@ -30,77 +37,82 @@
             </router-link>
           </form>
         </div>
-
-        <div class="gig-contact-seller">
-          <button class="gig-contact-seller-btn">Contact Seller</button>
+        <div class="contact-seller-wrapper">
+          <div class="gig-contact-seller">
+            <span class="gig-contact-seller-btn">Contact Seller</span>
+            <arrowDown class="arrow-down" />
+          </div>
         </div>
       </section>
       <div class="gig-main">
         <div class="gig-overview">
-          <nav class="gig-categories-breadcrumbs">
-            <ul>
-              <li>
-                <span class="gig-category-breadcrumbs">
+          <div class="gig-details-header">
+            <nav class="gig-categories-breadcrumbs">
+              <ul>
+                <li>
                   <a href="" class="gig-category-link">category</a>
-                </span>
-                <span class="gig-breadcrumb-icon"> </span>
-              </li>
-              <li>
-                <span class="gig-category-breadcrumbs">
-                  <a href="" class="gig-category-link">subCategory</a>
-                </span>
-              </li>
-            </ul>
-          </nav>
-          <h1 class="gig-text-display">
-            {{ gig.title }}
-          </h1>
-          <div class="gig-seller-overview">
-            <div class="gig-seller-pic">
-              <!-- //import picture from database/json to here -->
-              <img
-                class="B9j8CWn"
-                src="https://fiverr-res.cloudinary.com/t_profile_thumb,q_auto,f_auto/attachments/profile/photo/851682dc8f4fb93905e5db125ce56655-1552635815584/83c9dc85-a0fd-4bcc-a0e5-b6a3e886ca77.jpg"
-                alt="raziul99"
-                loading="lazy"
-              />
-            </div>
-            <div class="gig-seller-details">
-              <div class="gig-seller-name">{{ gig.fullname }}</div>
-              <span class="gig-seller-level"
-                ><div class="gig-seller-level-div">Level 2 Seller</div></span
-              >
-              <div class="gig-seller-rating">
-                <div class="gig-seller-stars">
-                  <span class="gig-seller-star"></span
-                  ><span class="gig-seller-star"></span
-                  ><span class="gig-seller-star"></span
-                  ><span class="gig-seller-star"></span
-                  ><span class="gig-seller-star"></span>
-                </div>
-                <b class="gig-seller-rating-score">4.9</b
-                ><span class="gig-seller-ratings-count">(194)</span>
+                  <arrowRight />
+                </li>
+                <li>
+                  <a href="" class="gig-category-link">sub category</a>
+                  <arrowRight />
+                </li>
+              </ul>
+            </nav>
+            <h1 id="overview" class="gig-text-display">
+              {{ gig.title }}
+            </h1>
+            <div class="gig-seller-overview">
+              <div class="gig-seller-pic">
+                <!-- //import picture from database/json to here -->
+                <img
+                  class="B9j8CWn"
+                  src="https://fiverr-res.cloudinary.com/t_profile_thumb,q_auto,f_auto/attachments/profile/photo/851682dc8f4fb93905e5db125ce56655-1552635815584/83c9dc85-a0fd-4bcc-a0e5-b6a3e886ca77.jpg"
+                  alt="raziul99"
+                  loading="lazy"
+                />
               </div>
-              <div class="gig-seller-order-queue">10 Orders in Queue</div>
+              <div class="gig-seller-details">
+                <div class="gig-seller-name">{{ gig.fullname }}</div>
+                <span class="gig-seller-level"
+                  ><div class="gig-seller-level-div">Level 2 Seller</div></span
+                >
+                <div class="gig-seller-rating">
+                  <div class="gig-seller-stars">
+                    <span class="gig-seller-star"></span
+                    ><span class="gig-seller-star"></span
+                    ><span class="gig-seller-star"></span
+                    ><span class="gig-seller-star"></span
+                    ><span class="gig-seller-star"></span>
+                  </div>
+                  <b class="gig-seller-rating-score">4.9</b
+                  ><span class="gig-seller-ratings-count">(194)</span>
+                </div>
+                <div class="gig-seller-order-queue">10 Orders in Queue</div>
+              </div>
             </div>
           </div>
         </div>
         <section class="gig-gallery-component">
           <gig-details-gallery-carousel :gig="gig" />
         </section>
-        <section class="gig-reviews-carousel-component">
+        <section id="reviews" class="gig-reviews-carousel-component">
           <gig-details-reviews-carousel :gig="gig" />
         </section>
 
-        <div class="gig-description">
-          <header><h2 class="gig-section-title">About This Gig</h2></header>
+        <div id="gig-description" class="gig-description">
+          <header>
+            <h2  class="gig-section-title">
+              About This Gig
+            </h2>
+          </header>
           <div class="gig-description-content">
             <p>
               {{ gig.description }}
             </p>
           </div>
         </div>
-        <div class="gig-about-seller-wrapper">
+        <div id="seller-description" class="gig-about-seller-wrapper">
           <gig-about-seller :gig="gig" />
         </div>
       </div>
@@ -109,59 +121,91 @@
 </template>
 
 <script>
-  import { gigService } from "../services/gig-service";
-  import gigDetailsGalleryCarousel from "../components/gig-details-gallery-carousel.vue";
-  import gigDetailsReviewsCarousel from "../components/gig-details-reviews-carousel.vue";
-  import gigAboutSeller from "../components/gig-about-seller.vue";
-  export default {
-    components: {
-      gigDetailsGalleryCarousel,
-      gigDetailsReviewsCarousel,
-      gigAboutSeller,
+import { gigService } from "../services/gig-service";
+import gigDetailsGalleryCarousel from "../components/gig-details-gallery-carousel.vue";
+import gigDetailsReviewsCarousel from "../components/gig-details-reviews-carousel.vue";
+import gigAboutSeller from "../components/gig-about-seller.vue";
+import arrowRight from "../svgs/arrow-right.vue";
+import arrowDown from "../svgs/arrow-down.vue";
+export default {
+  components: {
+    gigDetailsGalleryCarousel,
+    gigDetailsReviewsCarousel,
+    gigAboutSeller,
+    arrowRight,
+    arrowDown,
+  },
+  data() {
+    return {
+      gig: null,
+      limitPosition: 120,
+      isSticky: false,
+      lastPosition: 0,
+      currSection: null,
+      gigDetailsNavLinks: [
+        {
+          name: "Overview",
+          section: "overview",
+          active: false,
+        },
+        {
+          name: "Description",
+          section: "gig-description",
+          active: false,
+        },
+        {
+          name: "About The Seller",
+          section: "seller-description",
+          active: false,
+        },
+        {
+          name: "Reviews",
+          section: "reviews",
+          active: false,
+        },
+      ],
+    };
+  },
+  created() {
+    this.loadGig();
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  unmounted() {
+    window.removeEventListener("scroll", this.handleScroll);
+    this.loadGig();
+  },
+  computed: {
+    async reviewsLength() {
+      return await this.gig.reviewers.length;
     },
-    data() {
-      return {
-        gig: null,
-        limitPosition: 120,
-        scrolled: false,
-        lastPosition: 0,
-      };
+    loggedInUser() {
+      return this.$store.getters.loggedinUser && "please login";
     },
-    created() {
-      this.loadGig();
-      window.addEventListener("scroll", this.handleScroll);
+
+    setSticky() {
+      return this.isSticky
+        ? { position: "fixed", top: "0" }
+        : { position: "absolute", top: 120 + "px" };
     },
-    unmounted() {
-      window.removeEventListener("scroll", this.handleScroll);
-      this.loadGig();
+  },
+  methods: {
+    async loadGig() {
+      const { id } = this.$route.params;
+      this.gig = await gigService.getById(id);
     },
-    computed: {
-      async reviewsLength() {
-        return await this.gig.reviewers.length;
-      },
-      loggedInUser() {
-        return this.$store.getters.loggedinUser && "please login";
-      },
+    handleScroll() {
+      if (window.scrollY > 120) this.isSticky = true;
+      else this.isSticky = false;
     },
-    methods: {
-      async loadGig() {
-        const { id } = this.$route.params;
-        this.gig = await gigService.getById(id);
-      },
-      handleScroll() {
-        if (
-          this.lastPosition < window.scrollY &&
-          this.limitPosition < window.scrollY
-        ) {
-          this.scrolled = true;
-        }
-        if (this.lastPosition > window.scrollY) {
-          this.scrolled = false;
-        }
-        this.lastPosition = window.scrollY;
-      },
+    scrollTo(pos,idx) {
+      this.gigDetailsNavLinks.forEach((link) => link.active = false)
+      this.gigDetailsNavLinks[idx].active = true
+      document.getElementById(pos).scrollIntoView({
+        behavior: "smooth",
+      });
     },
-  };
+  },
+};
 </script>
 
 <style></style>
--- src/views/gig-purchase-page.vue
+++ src/views/gig-purchase-page.vue
@@ -1,96 +1,152 @@
 <template>
   <div class="page-content-container">
     <section v-if="gig" class="gig-checkout-page gig-page">
-      <div class="gig-order-preview">
-        <p>seller : {{ gig.fullname }}</p>
-        <img :src="gig.productImgs[0]" alt="" />
-        <p>gig title: {{ gig.title }}</p>
-        <p>gig rate:{{ gig.rate }}</p>
-        <p>gig reviews:{{ reviewsLength }}</p>
-        <p>price: ({{ gig.price }})</p>
+      <div class="gig-order-summary">
+        <div class="gig-order-preview">
+          <section class="block-two">
+            <img :src="gig.productImgs[0]" alt="" />
+            
+          </section>
+          <section class="block-one">
+            <h1>{{ gig.title }}</h1>
+            <p>{{ reviewsLength }} reviews</p>
+            <div>
+              <p>{{ gig.fullname }}</p>
+              <div class="gig-rate">
+                <p>{{ gig.rate }}</p>
+                <ul>
+                  <li v-for="starToRender in starsToRender" :key="starToRender">
+                    <Star />
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <h3>{{ gig.price }}</h3>
+          </section>
+        </div>
+        <div class="gig-order-details">
+          <h1 class="details-title">Order Details</h1>
+          <ul class="gig-features">
+            <li
+              class="gig-feature"
+              v-for="feature in gigFeaturesList"
+              :key="feature"
+            >
+              <Checkmark />
+              <p class="feature-desc">{{ feature }}</p>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="gig-checkout-tab">
-        <form class="gig-purchase-content">
-          <p>price: ({{ gig.price }})</p>
-          <p>-serive fee {{ serviceFee }}</p>
-          <p>final price {{ finalPrice }}</p>
+      <div class="gig-checkout-summary">
+          <h1 class="title">Price Summary</h1>
+          <li>
+            <p>Subtotal</p>
+          <p>{{ gig.price }}</p>
+          </li>
+          <li>
+            <p>Service Fee</p>
+          <p>{{ serviceFee }}</p>
+          </li>
+          <li>
+            <p>Final Price</p>
+          <p>{{ finalPrice }}</p>
+          </li>
           <button @click.prevent="purchase" v-if="loggedInUser">
             Purchase
           </button>
-        </form>
+          <button v-else>Log In To Purchase!</button>
+
       </div>
     </section>
   </div>
 </template>
 
 <script>
-  import { gigService } from "../services/gig-service";
-  import { orderService } from "../services/order-service";
-  export default {
-    data() {
-      return {
-        gig: null,
-        order: null,
-        serviceFee: 5.92,
+import { gigService } from "../services/gig-service";
+import { orderService } from "../services/order-service";
+import Checkmark from "../svgs/check-mark.vue";
+import Star from "../svgs/star.vue";
+export default {
+  data() {
+    return {
+      gig: null,
+      order: null,
+      serviceFee: 5.92,
+      gigFeaturesList: [
+        "Commercial Use",
+        "Color",
+        "Source File",
+        "High Resoultion",
+        "Background/Scene",
+      ],
+    };
+  },
+  async created() {
+    this.loadGig();
+  },
+  computed: {
+    loggedInUser() {
+      return this.$store.getters.loggedinUser;
+    },
+    reviewsLength() {
+      return this.gig.reviewers.length;
+    },
+    finalPrice() {
+      const finalPrice = this.gig.price - this.serviceFee;
+      return finalPrice;
+    },
+
+    starsToRender() {
+      return Math.floor(this.gig.rate + 1);
+    },
+  },
+  methods: {
+    async loadGig() {
+      const { id } = this.$route.params;
+      this.gig = await gigService.getById(id);
+      this.order = await orderService.getEmptyOrder();
+    },
+    async purchase() {
+      console.log("purchase button");
+      const order = JSON.parse(JSON.stringify(this.order));
+      order.title = this.gig.title;
+      order.createdAt = Date.now();
+      order.buyer = {
+        _id: this.loggedInUser,
+        fullname: this.loggedInUser,
       };
+      order.seller = {
+        _id: "the ID of the SELLER",
+        fullname: this.gig.fullname,
+      };
+      order.gig = {
+        _id: this.gig._id,
+        title: this.gig.title,
+      };
+      console.log(order);
+      debugger;
+      await this.$store.dispatch({ type: "addOrder", order });
     },
-    async created() {
-      this.loadGig();
-    },
-    computed: {
-      loggedInUser() {
-        return this.$store.getters.loggedinUser;
-      },
-      reviewsLength() {
-        return this.gig.reviewers.length;
-      },
-      finalPrice() {
-        const finalPrice = this.gig.price - this.serviceFee;
-        return finalPrice;
-      },
-    },
-    methods: {
-      async loadGig() {
-        const { id } = this.$route.params;
-        this.gig = await gigService.getById(id);
-        this.order = await orderService.getEmptyOrder();
-      },
-      async purchase() {
-        console.log("purchase button");
-        const order = JSON.parse(JSON.stringify(this.order));
-        order.title = this.gig.title;
-        order.createdAt = Date.now();
-        order.buyer = {
-          _id: this.loggedInUser,
-          fullname: this.loggedInUser,
-        };
-        order.seller = {
-          _id: "the ID of the SELLER",
-          fullname: this.gig.fullname,
-        };
-        order.gig = {
-          _id: this.gig._id,
-          title: this.gig.title,
-        };
-        console.log(order);
-        debugger;
-        await this.$store.dispatch({ type: "addOrder", order });
-      },
-    },
-  };
+  },
+  components: {
+    Checkmark,
+    Star,
+  },
+};
 </script>
 
 <style>
-  .gig-purchase-content {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    border: 1px solid #ddd;
-    padding: 20px;
-    box-sizing: border-box;
-    position: relative;
-  }
-  .gig-order-preview {
-    width: 66%;
-  }
+.gig-purchase-content {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  border: 1px solid #ddd;
+  padding: 20px;
+  box-sizing: border-box;
+  position: relative;
+}
+.gig-order-preview {
+  width: 66%;
+}
 </style>
