dee55065daddff3f2908b0601f055ed17faf5e76
sowhat1234 <emiliu1234e@gmail.com>
2022-04-05 10:57:57 +0300

    speech fix

--- src/views/logged-out-home-page.vue
+++ src/views/logged-out-home-page.vue
@@ -116,153 +116,152 @@
 </template>
 
 <script>
-import searchIconVue from "../svgs/search-icon.vue";
-import ServicesIcon from "../svgs/services-icon.vue";
-import SpeechIcon from "../svgs/microphone-icon.vue";
-import RecordingIcon from "../svgs/recording-icon.vue";
+  import searchIconVue from "../svgs/search-icon.vue";
+  import ServicesIcon from "../svgs/services-icon.vue";
+  import SpeechIcon from "../svgs/microphone-icon.vue";
+  import RecordingIcon from "../svgs/recording-icon.vue";
 
-export default {
-  data() {
-    return {
-      inputVal: "",
-      isRecording: false,
-      transcription: [],
-      // inputVal: [
-      //   {speech:''},{txt:''}
-      //   ],
-      // categoryies:['logo','arts and crafts','research and summeries','data entry','marketing','business','programming and tech'],
-      heroIdx: 0,
-      heroTimeout: null,
-      categories: [
-        {
-          name: "Digital Marketing",
-          route: "/tag/digital marketing",
-          searchCount: 5,
-        },
-        {
-          name: "Research And Marketing",
-          route: "/tag/research and marketing",
-          searchCount: 4,
-        },
-        {
-          name: "Logo Design",
-          route: "/tag/logo",
-          searchCount: 3,
-        },
-        {
-          name: "Data Analysis",
-          route: "/tag/data analysis",
-          searchCount: 5,
-        },
-        {
-          name: "Programming And Tech",
-          route: "/tag/arts and crafts",
-          searchCount: 4,
-        },
-        {
-          name: "Business",
-          route: "/tag/fiiver business",
-          searchCount: 5,
-        },
-      ],
-    };
-  },
-
-  computed: {
-    getPopularCategories() {
-      return this.categories.sort((a, b) => b.searchCount - a.searchCount);
+  export default {
+    data() {
+      return {
+        inputVal: "",
+        isRecording: false,
+        transcription: [],
+        // inputVal: [
+        //   {speech:''},{txt:''}
+        //   ],
+        // categoryies:['logo','arts and crafts','research and summeries','data entry','marketing','business','programming and tech'],
+        heroIdx: 0,
+        heroTimeout: null,
+        categories: [
+          {
+            name: "Digital Marketing",
+            route: "/tag/digital marketing",
+            searchCount: 5,
+          },
+          {
+            name: "Research And Marketing",
+            route: "/tag/research and marketing",
+            searchCount: 4,
+          },
+          {
+            name: "Logo Design",
+            route: "/tag/logo",
+            searchCount: 3,
+          },
+          {
+            name: "Data Analysis",
+            route: "/tag/data analysis",
+            searchCount: 5,
+          },
+          {
+            name: "Programming And Tech",
+            route: "/tag/arts and crafts",
+            searchCount: 4,
+          },
+          {
+            name: "Business",
+            route: "/tag/fiiver business",
+            searchCount: 5,
+          },
+        ],
+      };
     },
 
-    setFilteredCategories() {
-      if (!this.inputVal) return false;
-      const regex = new RegExp(this.inputVal, "i");
-      return this.categories.filter((category) => regex.test(category.name));
+    computed: {
+      getPopularCategories() {
+        return this.categories.sort((a, b) => b.searchCount - a.searchCount);
+      },
+
+      setFilteredCategories() {
+        if (!this.inputVal) return false;
+        const regex = new RegExp(this.inputVal, "i");
+        return this.categories.filter((category) => regex.test(category.name));
+      },
+
+      // filteredCategories() {
+      //   if (!this.inputVal) return false;
+
+      //   const regex = new RegExp(this.inputVal, "i");
+      //   return this.categories.filter((category) => regex.test(category.name));
+      // },
     },
 
-    // filteredCategories() {
-    //   if (!this.inputVal) return false;
-
-    //   const regex = new RegExp(this.inputVal, "i");
-    //   return this.categories.filter((category) => regex.test(category.name));
-    // },
-  },
-
-  mounted() {
-    this.elHeroWrappers = document.querySelector(".hero-wrappers");
-    this.heroAnimation();
-  },
-
-  umounted() {
-    clearTimeout(this.heroTimeout);
-  },
-
-  methods: {
-    startTxtToSpeech() {
-      window.SpeechRecognition =
-        window.SpeechRecognition || window.webkitSpeechRecognition;
-
-      const recognition = new SpeechRecognition();
-      recognition.interimResults = true;
-      recognition.lang = "en-US";
-
-      this.isRecording = true;
-      recognition.addEventListener("result", (e) => {
-        const transcript = Array.from(e.results)
-          .map((result) => result[0])
-          .map((result) => result.transcript)
-          .join("");
-        this.inputVal = transcript;
-        setTimeout(() => {
-          this.isRecording = false;
-        }, 2000);
-        setTimeout(() => {
-          if (
-            transcript === "logo" ||
-            transcript === "logo design" ||
-            transcript === "design"
-         ) {
-
-           this.$router.push("/tag/logo");
-         } else {
-           return this.$router.push("/tag/logo"); // only for real time show time.
-         }
-    
-          console.log("entered");
-          // else if(transcript)
-          if (transcript === "Yosef") window.open("https://www.pwp.co.il/", "_blank");
-          this.$store.dispatch({type:'isLoading', isLoading:true})
-        }, 2500);
-      });
-
-      // recognition.addEventListener('end', recognition.start);
-      recognition.addEventListener("end", () => {
-        setTimeout(() => {
-          this.transcription.push(this.inputVal);
-          this.inputVal = "";
-          this.isRecording = false;
-          recognition.stop();
-        }, 1500);
-      });
-      recognition.start();
+    mounted() {
+      this.elHeroWrappers = document.querySelector(".hero-wrappers");
+      this.heroAnimation();
     },
-    heroAnimation() {
-      if (this.$route.path !== "/") return;
-      const elHeroWrappers = document.querySelectorAll(".hero-wrapper");
-      if (this.heroIdx > 4) this.heroIdx = 0;
-      elHeroWrappers.forEach((hero) => (hero.style.opacity = "0"));
-      elHeroWrappers[this.heroIdx].style.opacity = "1";
-      this.heroIdx++;
-      this.heroTimeout = setTimeout(this.heroAnimation, 6000);
-    },
-  },
 
-  components: {
-    searchIconVue,
-    ServicesIcon,
-    SpeechIcon,
-    RecordingIcon,
-  },
-};
+    umounted() {
+      clearTimeout(this.heroTimeout);
+    },
+
+    methods: {
+      startTxtToSpeech() {
+        window.SpeechRecognition =
+          window.SpeechRecognition || window.webkitSpeechRecognition;
+
+        const recognition = new SpeechRecognition();
+        recognition.interimResults = true;
+        recognition.lang = "en-US";
+
+        this.isRecording = true;
+        recognition.addEventListener("result", (e) => {
+          const transcript = Array.from(e.results)
+            .map((result) => result[0])
+            .map((result) => result.transcript)
+            .join("");
+          this.inputVal = transcript;
+          setTimeout(() => {
+            this.isRecording = false;
+          }, 2000);
+          setTimeout(() => {
+            if (
+              transcript === "logo" ||
+              transcript === "logo design" ||
+              transcript === "design"
+            ) {
+              this.$router.push("/tag/logo");
+            } else {
+              return this.$router.push("/tag/logo"); // only for real time show time.
+            }
+
+            console.log("entered");
+            // else if(transcript)
+            if (transcript === "Yosef")
+              window.open("https://www.pwp.co.il/", "_blank");
+          }, 2500);
+        });
+
+        // recognition.addEventListener('end', recognition.start);
+        recognition.addEventListener("end", () => {
+          setTimeout(() => {
+            this.transcription.push(this.inputVal);
+            this.inputVal = "";
+            this.isRecording = false;
+            recognition.stop();
+          }, 1500);
+        });
+        recognition.start();
+      },
+      heroAnimation() {
+        if (this.$route.path !== "/") return;
+        const elHeroWrappers = document.querySelectorAll(".hero-wrapper");
+        if (this.heroIdx > 4) this.heroIdx = 0;
+        elHeroWrappers.forEach((hero) => (hero.style.opacity = "0"));
+        elHeroWrappers[this.heroIdx].style.opacity = "1";
+        this.heroIdx++;
+        this.heroTimeout = setTimeout(this.heroAnimation, 6000);
+      },
+    },
+
+    components: {
+      searchIconVue,
+      ServicesIcon,
+      SpeechIcon,
+      RecordingIcon,
+    },
+  };
 </script>
 
 <style></style>
